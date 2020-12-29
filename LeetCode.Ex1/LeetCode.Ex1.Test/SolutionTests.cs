c06c594df12d53694de253d6f4aa06105031efe5
ubersteve <steve@ubersteve.com>
2020-12-29 21:30:28 +0000

    Added solution and test projects

---
+++ LeetCode.Ex1/LeetCode.Ex1.Test/SolutionTests.cs
@@ -0,0 +1,6 @@
+﻿namespace LeetCode.Ex1.Test
+{
+    public class SolutionTests
+    {
+    }
+}
---
+++ LeetCode.Ex1/LeetCode.Ex1/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.Ex1
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            return new int[] { };
+        }
+    }
+}
