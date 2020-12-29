9777792385c75661f35538034d84c61d7a35bf3f
ubersteve <steve@ubersteve.com>
2020-12-29 21:48:21 +0000

    Added tests

--- LeetCode.Ex1/LeetCode.Ex1.Test/SolutionTests.cs
+++ LeetCode.Ex1/LeetCode.Ex1.Test/SolutionTests.cs
@@ -1,6 +1,32 @@
-﻿namespace LeetCode.Ex1.Test
+﻿using Xunit;
+using Should;
+
+namespace LeetCode.Ex1.Test
 {
     public class SolutionTests
     {
+        [Theory]
+        [InlineData(new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 })]
+        [InlineData(new int[] { 3, 2, 4 }, 6, new int[] { 1, 2 })]
+        [InlineData(new int[] { 3, 3 }, 6, new int[] { 0, 1 })]
+        public void ShouldReturnCorrectResultWhenSolutionExists(int[] nums, int target, int[] expectedResult)
+        {
+            var actualResult = Solution.TwoSum(nums, target);
+
+            actualResult.ShouldEqual(expectedResult);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 2, 7, 11, 15 }, 1)]
+        [InlineData(new int[] { 2, 7, 11, 15 }, 100)]
+        [InlineData(new int[] { 3, 2, 4 }, 5)]
+        [InlineData(new int[] { 3, 2, 4 }, 6)]
+        public void ShouldReturnEmptyArrayWhenNoSolutionExists(int[] nums, int target)
+        {
+            var expectedResult = new int[] { };
+            var actualResult = Solution.TwoSum(nums, target);
+
+            actualResult.ShouldEqual(expectedResult);
+        }
     }
 }
--- LeetCode.Ex1/LeetCode.Ex1/Solution.cs
+++ LeetCode.Ex1/LeetCode.Ex1/Solution.cs
@@ -1,10 +1,12 @@
-﻿namespace LeetCode.Ex1
+﻿using System;
+
+namespace LeetCode.Ex1
 {
     public class Solution
     {
-        public int[] TwoSum(int[] nums, int target)
+        public static int[] TwoSum(int[] nums, int target)
         {
-            return new int[] { };
+            throw new NotImplementedException();
         }
     }
 }
